94bc52b70c59bcc0cff72ca6e59397af79723b70
Jan Havran <havran.jan@email.cz>
2017-06-01 16:09:39 +0200

    TXT: show file info in status bar

--- src/txt.cpp
+++ src/txt.cpp
@@ -8,20 +8,30 @@ TXT::TXT(std::string name)
 {
 	setDir(false);
 	setCompressed(false);
-	m_widget = new QTextEdit;
-	((QTextEdit *) m_widget)->setReadOnly(true);
+	m_widget = new QWidget;
+	m_layout = new QVBoxLayout;
+	m_edit = new QTextEdit;
+
+	m_edit->setReadOnly(true);
 	m_codec = QTextCodec::codecForName("Windows-1250");
+	m_status = new QStatusBar;
+	m_layout->addWidget(m_edit);
+	m_layout->addWidget(m_status);
+	m_widget->setLayout(m_layout);
 }
 
 void TXT::setData(uint8_t *data, size_t size)
 {
 	AbstractFile::setData(data, size);
 	m_str.clear();
+	m_status->clearMessage();
 
 	if (data != NULL)
 		m_str.append(m_codec->toUnicode((char *) data));
 
-	((QTextEdit *) m_widget)->setText(m_str);
+	m_edit->setText(m_str);
+	m_status->showMessage(QString("Bytes total: ") + QString::number(size) +
+		QString("; Chars total: ") + QString::number(m_str.size()));
 }
 
 QWidget *TXT::getWidget()
--- src/txt.h
+++ src/txt.h
@@ -3,6 +3,10 @@
 
 #include <string>
 
+#include <QTextEdit>
+#include <QStatusBar>
+#include <QVBoxLayout>
+
 #include "abstractfile.h"
 
 class TXT: public AbstractFile{
@@ -10,6 +14,9 @@ private:
 	QString m_str;
 	QWidget *m_widget;
 	QTextCodec *m_codec;
+	QTextEdit *m_edit;
+	QStatusBar *m_status;
+	QVBoxLayout *m_layout;
 public:
 	TXT(std::string name);
 	virtual void setData(uint8_t *data, size_t size);
