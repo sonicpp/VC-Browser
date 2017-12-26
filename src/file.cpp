e80d4b38c3140e346a81ffc08d3982ebec69ad89
Jan Havran <havran.jan@email.cz>
2017-12-26 18:51:37 +0100

    UnknownFile: code style, code clean-up

--- src/file.cpp
+++ src/file.cpp
@@ -1,9 +1,7 @@
-#include <QTextEdit>
-
 #include "file.h"
 
 UnknownFile::UnknownFile(QString name)
-:AbstractFile(false, name, NULL)
+:AbstractFile(false, name, NULL), m_size(0u)
 {
 	mp_layout = new QVBoxLayout;
 	mp_edit = new QTextEdit;
@@ -13,6 +11,8 @@ UnknownFile::UnknownFile(QString name)
 	mp_layout->addWidget(mp_edit);
 	mp_layout->addWidget(mp_status);
 	mp_widget->setLayout(mp_layout);
+
+	redrawWidget();
 }
 
 UnknownFile::~UnknownFile()
@@ -22,10 +22,19 @@ UnknownFile::~UnknownFile()
 	delete mp_layout;
 }
 
-bool UnknownFile::setData(uint8_t *data, size_t size, QProgressDialog *p_progress)
+bool UnknownFile::setData(uint8_t *p_data, size_t size,
+			  QProgressDialog *p_progress)
 {
-	mp_status->clearMessage();
-	mp_status->showMessage(QString("Bytes total: ") + QString::number(size));
+	m_size = size;
+
+	redrawWidget();
 
 	return false;
+}
+
+void UnknownFile::redrawWidget(void)
+{
+	mp_status->clearMessage();
+	mp_status->showMessage(QString("Bytes total: ") +
+			       QString::number(m_size));
 }
--- src/file.h
+++ src/file.h
@@ -1,21 +1,26 @@
 #ifndef _FILE_H
 #define _FILE_H
 
-#include <string>
 #include <QTextEdit>
 #include <QStatusBar>
 #include <QVBoxLayout>
+#include <QString>
+#include <QProgressBar>
+
 #include "abstractfile.h"
 
 class UnknownFile: public AbstractFile {
-private:
-	QTextEdit *mp_edit;
-	QStatusBar *mp_status;
-	QVBoxLayout *mp_layout;
 public:
 	UnknownFile(QString name);
 	~UnknownFile();
-	bool setData(uint8_t *data, size_t size, QProgressDialog *p_progress = NULL);
+	bool setData(uint8_t *p_data, size_t size,
+		     QProgressDialog *p_progress = NULL);
+private:
+	size_t m_size;
+	QTextEdit *mp_edit;
+	QStatusBar *mp_status;
+	QVBoxLayout *mp_layout;
+	void redrawWidget();
 };
 
-#endif
+#endif	// _FILE_H
