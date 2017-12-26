ee8de95d4dcf10009f8ead82da23150cf644fc16
Jan Havran <havran.jan@email.cz>
2017-12-26 12:45:30 +0100

    TXT: create descructor

--- src/txt.cpp
+++ src/txt.cpp
@@ -6,16 +6,23 @@
 TXT::TXT(QString name, AbstractFile *p_parent)
 :AbstractFile(false, name, p_parent)
 {
-	m_layout = new QVBoxLayout;
-	m_edit = new QTextEdit;
+	mp_layout = new QVBoxLayout;
+	mp_edit = new QTextEdit;
 
-	m_edit->setReadOnly(true);
-	m_codec = QTextCodec::codecForName("Windows-1250");
-	m_status = new QStatusBar;
-	m_layout->addWidget(m_edit);
-	m_layout->addWidget(m_status);
-	mp_widget->setLayout(m_layout);
+	mp_edit->setReadOnly(true);
+	mp_codec = QTextCodec::codecForName("Windows-1250");
+	mp_status = new QStatusBar;
+	mp_layout->addWidget(mp_edit);
+	mp_layout->addWidget(mp_status);
+	mp_widget->setLayout(mp_layout);
 
+}
+
+TXT::~TXT()
+{
+	delete mp_edit;
+	delete mp_status;
+	delete mp_layout;
 }
 
 bool TXT::setData(uint8_t *data, size_t size, QProgressDialog *p_progress)
@@ -23,15 +30,15 @@ bool TXT::setData(uint8_t *data, size_t size, QProgressDialog *p_progress)
 	char *buff;
 
 	m_str.clear();
-	m_status->clearMessage();
+	mp_status->clearMessage();
 
 	buff = new char[size + 1];
 	memcpy(buff, data, size);
 	buff[size] = '\0';
 
-	m_str = m_codec->toUnicode(buff);
-	m_edit->setText(m_str);
-	m_status->showMessage(QString("Bytes total: ") + QString::number(size) +
+	m_str = mp_codec->toUnicode(buff);
+	mp_edit->setText(m_str);
+	mp_status->showMessage(QString("Bytes total: ") + QString::number(size) +
 		QString("; Chars total: ") + QString::number(m_str.size()));
 
 	delete[] buff;
--- src/txt.h
+++ src/txt.h
@@ -9,12 +9,13 @@
 class TXT: public AbstractFile {
 private:
 	QString m_str;
-	QTextCodec *m_codec;
-	QTextEdit *m_edit;
-	QStatusBar *m_status;
-	QVBoxLayout *m_layout;
+	QTextCodec *mp_codec;
+	QTextEdit *mp_edit;
+	QStatusBar *mp_status;
+	QVBoxLayout *mp_layout;
 public:
 	TXT(QString name, AbstractFile *p_parent);
+	~TXT();
 	bool setData(uint8_t *data, size_t size, QProgressDialog *p_progress = NULL);
 };
 
